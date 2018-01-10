0e24119bbef39a4e3aef1b138fb40d191fd4de1d
Raghu Betina <raghu@firstdraft.com>
2018-01-10 00:53:16 +0000

    Only friend_request user can access show page

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,5 +1,5 @@
 class FriendRequestsController < ApplicationController
-  before_action :current_user_must_be_friend_request_user, :only => [:edit, :update, :destroy]
+  before_action :current_user_must_be_friend_request_user, :only => [:show, :edit, :update, :destroy]
 
   def current_user_must_be_friend_request_user
     friend_request = FriendRequest.find(params[:id])
